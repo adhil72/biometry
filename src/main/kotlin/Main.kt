cc9fd00b92ce7a46ba61d7832912f84e32c93fb8
adhil <adhil.mhdk28@gmail.com>
2024-09-22 18:09:51 +0530

    fs

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,13 @@
 package gecw.cse
 
 import gecw.ace.lumina.Lumina
+import gecw.ace.lumina.utils.Resource
+import gecw.cse.utils.Terminal
+import gecw.cse.utils.extractResource
 import gecw.cse.views.connection.Connecting
 import javafx.application.Application
 
-class App: Lumina(){
+class App : Lumina() {
     override fun onViewCreated() {
         super.onViewCreated()
         setView(Connecting())
@@ -12,6 +15,9 @@ class App: Lumina(){
 }
 
 fun main() {
-   Application.launch(App::class.java)
-    println("stated")
+    extractResource("driver","driver")
+    extractResource("libScanAPI.so","libScanAPI.so")
+    Terminal.executeCommand("chmod +x driver",false)
+    Terminal.executeCommand("chmod +x libScanAPI.so",false)
+    Application.launch(App::class.java)
 }
